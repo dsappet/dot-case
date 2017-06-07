3435de45146d5ce181276e229356a56461f99ae0
Douglas Sappet <dsappet@gmail.com>
2017-06-07 16:27:22 -0400

    Modified to work as Upper Dot Case and added edge test cases

--- test.js
+++ test.js
@@ -1,34 +1,44 @@
 /* global describe, it */
-var assert = require('assert')
-var dotCase = require('./')
+var assert = require('assert');
+var upperDotCase = require('./');
 
 describe('dot case', function () {
   it('should dot case a single word', function () {
-    assert.equal(dotCase('test'), 'test')
-    assert.equal(dotCase('TEST'), 'test')
+    assert.equal(upperDotCase('test'), 'Test');
+    assert.equal(upperDotCase('TEST'), 'Test');
   })
 
   it('should dot case regular sentence cased strings', function () {
-    assert.equal(dotCase('test string'), 'test.string')
-    assert.equal(dotCase('Test String'), 'test.string')
+    assert.equal(upperDotCase('test string'), 'Test.String');
+    assert.equal(upperDotCase('Test String'), 'Test.String');
   })
 
   it('should dot case non-alphanumeric separators', function () {
-    assert.equal(dotCase('dot.case'), 'dot.case')
-    assert.equal(dotCase('path/case'), 'path.case')
+    assert.equal(upperDotCase('dot.case'), 'Dot.Case');
+    assert.equal(upperDotCase('path/case'), 'Path.Case');
   })
 
   it('should dot case dot cased strings', function () {
-    assert.equal(dotCase('TestString'), 'test.string')
-    assert.equal(dotCase('TestString1_2_3'), 'test.string1.2.3')
-    assert.equal(dotCase('TestString_1_2_3'), 'test.string.1.2.3')
+    assert.equal(upperDotCase('TestString'), 'Test.String')
+    assert.equal(upperDotCase('TestString1_2_3'), 'Test.String1.2.3');
+    assert.equal(upperDotCase('TestString_1_2_3'), 'Test.String.1.2.3');
   })
 
   it('should support non-latin characters', function () {
-    assert.equal(dotCase('My Entrée'), 'my.entrée')
+    assert.equal(upperDotCase('My Entrée'), 'My.Entrée');
   })
 
   it('should support locales', function () {
-    assert.equal(dotCase('MY STRING', 'tr'), 'my.strıng')
+    assert.equal(upperDotCase('MY STRING', 'tr'), 'My.Strıng');
   })
+    
+    it('dot edge cases', function (done) {
+        try{
+    assert.equal(upperDotCase('.MY STRING'), 'My.String');
+        assert.equal(upperDotCase('MY STRING.'), 'My.String');
+        }catch(e){
+            done(e);
+        }
+  })
+    
 })
